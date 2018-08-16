0a0f440d128ace39260689d8614365fb67ee9595
Daniel Neis Araujo <danielneis@gmail.com>
2018-08-16 10:33:47 -0300

    bump version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2018081602; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018081603; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018050800; // Requires this Moodle version.
 $plugin->component = 'tool_danielneis'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '2'; // incremental releases
+$plugin->release = '3'; // incremental releases.
